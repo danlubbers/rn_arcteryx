80c8b3e0d1f964684f465e2276af5fd60a857917
Dan Lubbers <lubbers.dan@gmail.com>
2021-11-05 15:17:07 -0400

    Setting up React Native with Navigation and assets

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+import Container from './src/components/Container/Container';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Container>
+        <Text>App</Text>
+      </Container>
+    </NavigationContainer>
+  );
+};
+
+export default App;
